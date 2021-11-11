d54f5dd4315b00ace0f00c3ece5cdf32adf808fa
Vaibhav Shinde <vaibhavshinde1642@gmail.com>
2021-11-11 23:04:13 +0530

    new spring boot project added

---
+++ src/main/java/com/ashokit/Jrtp05GitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jrtp05GitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jrtp05GitDemoApplication.class, args);
+	}
+
+}
