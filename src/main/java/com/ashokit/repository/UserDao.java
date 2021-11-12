8762bd289e4aeb74f7c86f113d729fa849398e67
Vaibhav Shinde <vaibhavshinde1642@gmail.com>
2021-11-12 11:22:30 +0530

    UserDao Modified

--- src/main/java/com/ashokit/repository/UserDao.java
+++ src/main/java/com/ashokit/repository/UserDao.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ashokit.beans.User;
 
 public interface UserDao extends JpaRepository<User, Integer> {
-
+	/**
+	 * 
+	 * unfinished work
+	 */
 }
