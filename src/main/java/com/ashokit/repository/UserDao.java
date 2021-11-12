ca567ff72bae79a0a8f4fa740eefdb4dbdd520cc
Vaibhav Shinde <vaibhavshinde1642@gmail.com>
2021-11-12 11:11:28 +0530

    UserDao created

---
+++ src/main/java/com/ashokit/repository/UserDao.java
@@ -0,0 +1,9 @@
+package com.ashokit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ashokit.beans.User;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+
+}
